9b48e811589afc06882b5714f79bfb00239818d9
Ali Aliyev <shamkir@gmail.com>
2015-08-11 17:04:02 +0300

    added description field and migration

--- djstripe/models.py
+++ djstripe/models.py
@@ -1063,7 +1063,7 @@ class Plan(StripeObject):
                                  null=False)
     trial_period_days = models.IntegerField(null=True)
 
-    description = models.TextField()
+    description = models.TextField(description="Description of the plans. Description through a new line")
 
     def __str__(self):
         return self.name
