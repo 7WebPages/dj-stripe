87345ddf101e0e4379a21ca6c801e2b33c6f1841
Ali Aliyev <shamkir@gmail.com>
2015-08-31 19:44:00 +0500

    added ability to send invoices after success transaction

--- djstripe/models.py
+++ djstripe/models.py
@@ -558,7 +558,7 @@ class Customer(StripeObject):
     def sync_invoices(self, cu=None, **kwargs):
         cu = cu or self.stripe_customer
         for invoice in cu.invoices(**kwargs).data:
-            Invoice.sync_from_stripe_data(invoice, send_receipt=SEND_INVOICE_RECEIPT_EMAILS)
+            Invoice.sync_from_stripe_data(invoice, send_receipt=True)
 
     def sync_charges(self, cu=None, **kwargs):
         cu = cu or self.stripe_customer
