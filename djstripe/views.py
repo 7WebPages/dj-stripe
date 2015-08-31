10b3fce2bc1ceb23a6d2ee2cc1984b8bc33c92a1
Yuri Kriachko <yuriykryachko@gmail.com>
2015-08-31 12:58:42 +0300

    up

--- djstripe/views.py
+++ djstripe/views.py
@@ -293,6 +293,7 @@ class HistoryView(LoginRequiredMixin,
     object = Invoice
 
     def get_queryset(self):
+        sync_customer(request.user)
         queryset = super(SelectRelatedMixin, self).get_queryset()
         config = RequestConfig(self.request)
         user = User.objects.get(pk=self.request.user.id)
